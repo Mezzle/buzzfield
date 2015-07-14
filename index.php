ed0136ab8c3ad50eda2de64157d2280b37f3660f
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2015-07-14 16:13:44 +0100

    Update positioning and font

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
-    <link href='//fonts.googleapis.com/css?family=Coming+Soon' rel='stylesheet' type='text/css'>
+    <title>BuzzField</title>
+    <link href='http://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister' rel='stylesheet' type='text/css'>
     <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
     <script>
-        $(document).ready(function() {
-            $.getJSON('/text.json', function(data) {
+        $(document).ready(function () {
+            $.getJSON('/text.json', function (data) {
                 //console.log(data);
                 var rand = data[Math.floor(Math.random() * data.length)];
 
@@ -25,17 +25,42 @@
             -o-background-size: cover;
             background-size: cover;
         }
+
+        body {
+            margin: 0;
+        }
+        .outer {
+            display: table;
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            margin: 0;
+            padding: 0;
+        }
+
+        .middle {
+            display: table-cell;
+            vertical-align: middle;
+            margin: 0;
+            padding: 0;
+        }
+
         #title {
-            font-family: "Coming Soon";
-            font-size: 48pt;
-            //color: white;
-            margin: 0 auto;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+            font-family: "Love Ya Like A Sister";
+            font-size: 60pt;
+            color: #555;
             text-align: center;
-            margin-top: 20%;
         }
     </style>
 </head>
 <body>
-<div id="title"></div>
+<div class="outer">
+    <div class="middle">
+        <div id="title"></div>
+    </div>
+</div>
 </body>
 </html>
